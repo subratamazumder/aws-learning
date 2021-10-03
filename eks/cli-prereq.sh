878854e133b9c9e8ebbd10f98d43a698c029f7a8
Subrata Mazumder <subrata.besu@gmail.com>
2021-10-03 16:51:54 +0100

    readme broken into sub sections

---
+++ eks/cli-prereq.sh
@@ -0,0 +1,8 @@
+export AWS_DEFAULT_PROFILE=devtest
+export AWS_DEFAULT_REGION=eu-west-2
+export MY_EKS_CLUSTER=eks-eprescription-poc
+export MY_EKS_KEYPAIR=eks-poc-keypair
+export MY_EKS_PUB_NODE_GROUP1=eks-eprescription-poc-ng-pub1
+export MY_EKS_PRV_NODE_GROUP1=eks-eprescription-poc-ng-prv1
+export MY_EKS_NODE1_EXTERNAL_IP=set-as-per-get-nodes-ip
+export MY_IP=`curl -s http://whatismyip.akamai.com/`
